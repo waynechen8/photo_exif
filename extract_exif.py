0a423a5ed0fbc38258b0aa84f88337aa4a1845ae
wayne <Wayne4_Chen@asus.com>
2022-08-27 13:13:24 +0800

    get exif from a heic photo

--- extract_exif.py
+++ extract_exif.py
@@ -1,5 +1,23 @@
-import re 
+from PIL import Image
 from PIL.Image import Exif
+from PIL.ExifTags import TAGS, GPSTAGS
 
-def get_exif():
-    pass
+def get_exif(file_name) -> Exif:
+    image: Image.Image = Image.open(file_name)
+    return image.getexif()
+
+
+def get_geo(exif):
+    for key, value in TAGS.items():
+        if value == "GPSInfo":
+            break
+    gps_info = exif.get_ifd(key)
+    return {
+        GPSTAGS.get(key, key): value
+        for key, value in gps_info.items()
+    }
+
+if __name__ == '__main__':
+    exif = get_exif("IMG_1754.HEIC")
+    geo = get_geo(exif)
+    print(geo)
